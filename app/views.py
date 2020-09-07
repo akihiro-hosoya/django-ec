b95b7bc1a57f2749a2e965772abc262faf93e05a
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-09-07 23:38:09 +0900

    商品詳細ページ

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+from .models import Item
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(ListView):
+    model = Item
     template_name = "app/index.html"
+    login_url = '/accounts/login/'
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'app/product.html'
     login_url = '/accounts/login/'
