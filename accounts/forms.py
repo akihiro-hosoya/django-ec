625e57c30a47992d7477ff5c1046576077883285
akihiro-hosoya <iicina.yftj.33@gmail.com>
2020-09-07 23:17:12 +0900

    Item登録

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,7 +12,6 @@ class SignupUserForm(SignupForm):
         user.save()
         return user
 
-
 class ProfileForm(forms.Form):
     first_name = forms.CharField(max_length=30, label='姓')
     last_name = forms.CharField(max_length=30, label='名')
